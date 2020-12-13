1fd6dade050089737f160aea77b342a416b1264e
Obinna Ezekwe <ozekwe@gmail.com>
2020-12-13 17:19:20 -0600

    Write and storing notes in db.json

--- Server.js
+++ Server.js
@@ -1,9 +1,12 @@
 const fs= require('fs');
+const {v4 : uuidv4} = require('uuid')
 const path= require('path');
 const express= require('express');
 const app= express();
-const notes= require('./db/db');
+const notes= require('./db/db.json');
+const { json } = require('express');
 const port= 3001;
+
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
@@ -14,11 +17,21 @@ app.get('/notes', (req, res)=> {
 });
 
 app.get('/api/notes', (req,res)=> {
-    res.json(notes);
+    res.send(notes);
 });
 
 app.post('/api/notes', (req,res)=> {
-
+    req.body.id= uuidv4();
+    const note= {title: req.body.title, text: req.body.text, id: req.body.id};
+    fs.readFile('./db/db.json', (err, Dtext)=>{
+        if(err) throw err;
+        let collectionNotes= JSON.parse(Dtext);
+        collectionNotes.push(note);
+        fs.writeFile('./db/db.json', JSON.stringify(collectionNotes,null,2), err=>{
+            if(err) throw err;
+            res.send(notes);
+        });
+    });
 });
 
 
