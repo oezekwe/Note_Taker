64eacb66cb50c50651150e93d4dc5c6fcea25c35
Obinna Ezekwe <ozekwe@gmail.com>
2020-12-13 21:03:03 -0600

    changed port number

--- Server.js
+++ Server.js
@@ -6,16 +6,11 @@ const express= require('express');
 const app= express();
 const notes= require('./db/db.json');
 const { json } = require('express');
-const port= 3001;
-
+const port= process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
-
-app.get('/', (req,res)=> {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
 
 app.get('/notes', (req, res)=> {
     res.sendFile(path.join(__dirname, './public/notes.html'));
